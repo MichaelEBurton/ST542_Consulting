91e12ab2090314f4f51341a2a8b8349e6980eabd
meburton19 <52686856+meburton19@users.noreply.github.com>
2020-02-11 14:32:25 -0500

    Plot Code/ to be commented...

--- Code/eda2.r
+++ Code/eda2.r
@@ -1,1 +1,21 @@
 # Create graphs
+require(reshape2)
+require(dplyr)
+require(stringr)
+require(ggplot2) 
+
+
+mdat <- melt(dat_condensed, id= c("Time", "Temp"))
+
+filter_by_row <- function(row_letter, data){
+  dat <- data %>% 
+           dplyr::filter(substr(data$variable, 1,1) == row_letter)
+  return(dat)
+}
+
+gdat <- filter_by_row("H", mdat)
+
+g <- ggplot(data = gdat)
+g <- g + geom_line(aes(x = Time, y = value, col = variable)) +
+     geom_hline(aes(yintercept = 0.2))
+g
