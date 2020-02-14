8d604ec43a20ce78077b5ad3312abb17caedf9a2
meburton19 <52686856+meburton19@users.noreply.github.com>
2020-02-14 12:19:53 -0500

    Update

--- Code/eda2.r
+++ Code/eda2.r
@@ -7,9 +7,40 @@
 ## Author: Michael Burton (meburton@ncsu.edu)                             #         
 ## Edited: 2/13/2020                                                      #   
 ##------------------------------------------------------------------------#
+require(ggplot2)
+require(dplyr)
 
+#========================================================
+## S Curves
 S_curve_by_row(dat, "C")
 
+#========================================================
+## Boxplots
+ctrl <- dplyr::arrange(reshape2::melt(control), Var1)
+ctrl[,2] <- "Control"
+ctrl[,4] <- paste0(ctrl[,2],ctrl[,1])
+colnames(ctrl) <- c("Row", "Column", "Value", "id")
+
+
+samp <- dplyr::arrange(reshape2::melt(sample), Var1)
+samp[,2] <- "Sample"
+samp[,4] <- paste0(samp[,2],samp[,1])
+colnames(samp) <- c("Row", "Column", "Value", "id")
+
+box_by_row <- dplyr::arrange(rbind(ctrl,samp), Row)
+
+ggplot(data = box_by_row) + 
+  geom_boxplot(aes(x = id, y =Value, color = id)) + 
+  xlab("Row & Specimen Id") +
+  ylab("Time to baseline (.2)") + 
+  theme_bw()
+
+#===================================================
+#Variance of control vs sample
+box_by_row %>% group_by(id) %>% summarise(Variance = sd(Value)^2)
+
+#===================================================
+## Unit Activity over time
 get_vectored <- unwind_activity(raw_activity)
 
 
@@ -20,10 +51,10 @@ b1 <- fit$coefficients[2]
 
 plot(fit)
 
-plot(1:36, get_vectored, type = "l", 
+plot(1:36, get_vectored, type = "l", lwd = 2, 
      main = "Raw Unit Activity over Time", xlab = "Well Order",
      ylab = "Raw Unit Activity")
-abline(b0, b1)
+abline(b0, b1, col = 'red', lwd = 2)
 # trend line doesn't change much when well 25 is removed
 
 acf(get_vectored)
