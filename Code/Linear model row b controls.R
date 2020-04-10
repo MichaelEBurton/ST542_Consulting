bcfef90a92264c9a520c18ad9700315a07fa0ea1
actomkin <actomkin@ncsu.edu>
2020-04-10 15:10:40 -0400

    edits

--- Code/Linear model row b controls.R
+++ Code/Linear model row b controls.R
@@ -1,6 +1,6 @@
 # Linear model for row by col effect only using controls in row B 
 
-interaction_data$arow <- c(rep(1,6),rep(2,6),rep(3,6),rep(4,6),rep(5,6),rep(6,6),rep(7,6))
+interaction_data$arow <- as.factor(c(rep(1,6),rep(2,6),rep(3,6),rep(4,6),rep(5,6),rep(6,6),rep(7,6)))
 
 #with interaction
 lin_mod1 <- lm(tinv ~ trt + arow + acol+arow:acol, data = interaction_data)
@@ -9,5 +9,5 @@ results1
 
 #without interaction
 lin_mod2 <- lm(tinv ~ trt + arow + acol, data = interaction_data)
-results2 <- anova(lin_mod1)
+results2 <- anova(lin_mod2)
 results2
